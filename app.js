229c1fb34df43646e1b99dae4a2f0167d24d89a3
Samuel Eras <sa.eras@gmx.de>
2023-10-17 07:41:56 +0200

    Sending images to Client

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
     switch (req.url) {
 
         //HOME SEITE
-        case '/':
+        case '/':                                       //HTML
             path += 'homeView/home.html';
             //TODO
             /* if(notLoggedIn){
@@ -21,19 +21,66 @@ const server = http.createServer((req, res) => {
                 res.end();
             } */
             break;
-        case '/style':
+        case '/style':                                  //CSS
             res.writeHead(200, {
                 "Content-Type": "text/css",
             });
             path += 'homeView/homeStyle.css';
             break;
-        case './homeView/bandScript':
+        case '/bandScript':                             //FrontendJS
             res.writeHead(200, {
                 "Content-Type": "text/javascript",
             });
             path += 'homeView/bandScript.js';
             break;
-        
+        case '/images/chicago':                         //Images
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/chicago.jpg';
+            break;
+        case '/images/ny':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/ny.jpg';
+            break;
+        case '/images/la':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/la.jpg';
+            break;
+        case '/images/bandmember':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/bandmember.jpg';
+            break;
+        case '/images/ny1':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/ny1.jpg';
+            break;
+        case '/images/paris1':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/paris1.jpg';
+            break;
+        case '/images/sf1':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/sf1.jpg';
+            break;
+        case '/images/map':
+            res.writeHead(200, {
+                "Content-Type": "image/jpeg",
+            });
+            path += 'homeView/images/map.jpg';
+            break;
 
         //LOGIN SEITE
         case '/login':
@@ -75,7 +122,6 @@ server.listen(3000, 'localhost', () => {
 });
 
 //TODO
-//Send images to Browser!
 //Read login file from app.js
 //Where to redirect to login of not logged in? From frontend or backend? Form sends directly to backend? Or form submit event is interupted and frontend sends data to backend with post, backend answers, frontend redirects, or backend redirects if correct. Probably backend...
 //Frontend checks with local storage if user is logged in, validates it with backend, otherwise backend redirects to login page if local storage userid doesnt exist in database (bzw file)
